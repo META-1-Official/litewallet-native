468ee4d2823b6dc0141ef5c43297bcb416e1bcab
Camisul <7963180+Camisul@users.noreply.github.com>
2021-10-06 01:58:04 +0300

    Add assets

---
+++ assets/index.ts
@@ -0,0 +1,5 @@
+export const coinAsset = require('./coin.png');
+export const logoAsset = require('./logo.png');
+
+export const marketingBsAssetOne = require('./graphics-img.png');
+export const marketingBsAssetTwo = require('./trade-traphics.png');
