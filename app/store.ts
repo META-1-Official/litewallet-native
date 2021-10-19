49e5d0e2b8d9f583d81251c47e7c39b68b7a6c2e
Camisul <7963180+Camisul@users.noreply.github.com>
2021-10-19 18:16:55 +0300

    Persistant stroage

--- app/store.ts
+++ app/store.ts
@@ -1,4 +1,6 @@
+import EncryptedStorage from 'react-native-encrypted-storage';
 import create from 'zustand';
+import { persist } from 'zustand/middleware';
 
 interface AppState {
   accountName: string;
@@ -6,8 +8,16 @@ interface AppState {
   authorize: (accountName: string) => void;
 }
 
-export const useStore = create<AppState>(set => ({
-  accountName: '',
-  authorized: false,
-  authorize: (accountName: string) => set({ accountName, authorized: true }),
-}));
+export const useStore = create<AppState>(
+  persist(
+    set => ({
+      accountName: '',
+      authorized: false,
+      authorize: (accountName: string) => set({ accountName, authorized: true }),
+    }),
+    {
+      name: 'app-storage',
+      getStorage: () => EncryptedStorage,
+    },
+  ),
+);
