070573af26232e5821c4922332a2ea75b436d517
Camisul <7963180+Camisul@users.noreply.github.com>
2021-11-05 21:59:51 +0300

    Preloader done

--- app/components/Loader.tsx
+++ app/components/Loader.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
-import { ActivityIndicator, SafeAreaView } from 'react-native';
+import { Dimensions, Image, SafeAreaView } from 'react-native';
+import { loaderGif } from '../../assets';
+
+const { width, height } = Dimensions.get('screen');
 
 const Loader: React.FC<{}> = () => {
   return (
-    <SafeAreaView style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-around' }}>
-      <ActivityIndicator />
+    <SafeAreaView
+      style={{
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+      }}
+    >
+      {/* <ActivityIndicator /> */}
+      <Image
+        style={{
+          width: width / 2,
+          height: height / 3,
+          resizeMode: 'contain',
+          marginTop: '30%',
+        }}
+        source={loaderGif}
+      />
     </SafeAreaView>
   );
 };
--- assets/index.ts
+++ assets/index.ts
@@ -9,6 +9,8 @@ export const ppIconAsset = require('./pp.png');
 export const tosIconAsset = require('./tos.png');
 export const personIconAsset = require('./personIcon.png');
 
+export const loaderGif = require('./loader.gif');
+
 export const SvgIcons = {
   fundAccount: require('./icons/fund-account.svg').default as any,
   settings: require('./icons/setting.svg').default as any,
