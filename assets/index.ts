1a5d1a833e74bc481db286a2b7a719ffcb8edfe7
Camisul <7963180+Camisul@users.noreply.github.com>
2021-11-09 23:59:38 +0300

    Upload icons

--- assets/index.ts
+++ assets/index.ts
@@ -10,10 +10,22 @@ export const tosIconAsset = require('./tos.png');
 export const personIconAsset = require('./personIcon.png');
 
 export const loaderGif = require('./loader.gif');
-
 export const SvgIcons = {
   fundAccount: require('./icons/fund-account.svg').default as any,
   settings: require('./icons/setting.svg').default as any,
   trading: require('./icons/trading.svg').default as any,
   wallet: require('./icons/wallet.svg').default as any,
+  BackArrowWhite: require('./icons/back-arrow-white.svg').default as any,
+  BuySellMeta1: require('./icons/buy-sell-meta1.svg').default as any,
+  Chart: require('./icons/chart.svg').default as any,
+  Exchange: require('./icons/exchange.svg').default as any,
+  FavoriteStar: require('./icons/favorite-star.svg').default as any,
+  GoldBackArrow: require('./icons/gold-back-arrow.svg').default as any,
+  Home: require('./icons/home.svg').default as any,
+  MyOrders: require('./icons/my-orders.svg').default as any,
+  OrdersTrade: require('./icons/orders-trade.svg').default as any,
+  Orders: require('./icons/orders.svg').default as any,
+  Trade: require('./icons/trade.svg').default as any,
+  TradesClock: require('./icons/trades-clock.svg').default as any,
+  Wallet: require('./icons/wallet.svg').default as any,
 };
