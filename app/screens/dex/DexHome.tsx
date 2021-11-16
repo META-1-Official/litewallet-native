40cbfe41642eefdd5146df91fc202fe0ed497549
Camisul <7963180+Camisul@users.noreply.github.com>
2021-11-16 16:57:39 +0300

    Dex home screen done

--- app/screens/dex/DexHome.tsx
+++ app/screens/dex/DexHome.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import { Image, SafeAreaView, Text, View } from 'react-native';
+import { FlatList, Image, SafeAreaView, Text, View, Dimensions } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 import { useAssets } from '../../utils/meta1Api';
 
+const { width, height } = Dimensions.get('screen');
 const DexHome: React.FC = () => {
   const accountAssets = useAssets();
   const assets = accountAssets!.assetsWithBalance;
+  console.log(JSON.stringify(assets[0]._asset, null, 4));
   return (
     <SafeAreaView
       style={{
@@ -36,6 +38,7 @@ const DexHome: React.FC = () => {
                   // padding: 8,
                   marginVertical: 12,
                 }}
+                key={`Asset_${e.symbol}`}
               >
                 <View style={{ flexDirection: 'row', width: 144 }}>
                   <Image
@@ -68,6 +71,54 @@ const DexHome: React.FC = () => {
               </View>
             );
           })}
+        </View>
+        <View style={{}}>
+          <Text style={{ color: '#fff', fontSize: 21, marginTop: 18, marginBottom: 24 }}>
+            New Crypto on META1
+          </Text>
+          <FlatList
+            data={assets.slice(0, 4)}
+            horizontal
+            pagingEnabled
+            renderItem={({ item, index }) => {
+              return (
+                <View
+                  key={`New_${index}`}
+                  style={{
+                    justifyContent: 'center',
+                    padding: 32,
+                    backgroundColor: '#1c1314',
+                    borderRadius: 24,
+                    width: width / 2.2,
+                    height: width / 2.2,
+                    marginRight: 8,
+                    alignItems: 'center',
+                  }}
+                >
+                  <Image
+                    source={item._asset.icon}
+                    style={{ width: 64, height: 64, resizeMode: 'contain' }}
+                  />
+                  <Text style={{ color: '#fff', fontSize: 16, fontWeight: '600' }}>
+                    {item.symbol}: $
+                    {item.usdt_value > 1000
+                      ? item.usdt_value.toFixed(0)
+                      : item.usdt_value.toFixed(2)}
+                  </Text>
+                  <Text
+                    style={{
+                      color: item.delta > 0 ? '#419e7f' : '#b02a27',
+                      fontSize: 24,
+                      fontWeight: 'bold',
+                    }}
+                  >
+                    {item.delta > 0 ? '+ ' : '- '}
+                    {Math.abs(item.delta)}%
+                  </Text>
+                </View>
+              );
+            }}
+          />
         </View>
       </ScrollView>
     </SafeAreaView>
