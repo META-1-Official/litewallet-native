faa06c79d70d2e849c192d52434e18f8bb076a4d
Camisul <7963180+Camisul@users.noreply.github.com>
2021-11-18 03:02:52 +0300

    Dex main modal rework

--- app/screens/dex/index.tsx
+++ app/screens/dex/index.tsx
@@ -10,6 +10,13 @@ import { SvgIcons } from '../../../assets';
 import DexHome from './DexHome';
 import DexTrade from './TradeScreen';
 import { OverlayContent } from '../../components/SideMenuOverlay';
+import {
+  createStackNavigator,
+  StackNavigationProp,
+  StackScreenProps,
+} from '@react-navigation/stack';
+import DexModal from '../../components/DexMainModal';
+
 const Black = () => <Loader bgc="#000" />;
 
 export type DexTabParamList = {
@@ -20,9 +27,16 @@ export type DexTabParamList = {
   DEX__Fund: undefined;
 };
 
-export type DexTabNavigationProp = BottomTabNavigationProp<DexStackParamList, 'DEX_HOME'>;
+export type DexTabNavigationProp = BottomTabNavigationProp<DexDrawerParamList, 'DEX_HOME'>;
+
+export type DexModalStackParamList = {
+  __Tabs: undefined;
+  __Modal: undefined;
+};
+export type DexModalStackNavigationProp = StackNavigationProp<DexModalStackParamList, '__Tabs'>;
 
 const Tab = createBottomTabNavigator<DexTabParamList>();
+const Stack = createStackNavigator<DexModalStackParamList>();
 
 const Name2Icon: Record<keyof DexTabParamList, any> = {
   DEX__Home: SvgIcons.Home,
@@ -31,60 +45,71 @@ const Name2Icon: Record<keyof DexTabParamList, any> = {
   DEX__Orders: SvgIcons.Orders,
   DEX__Fund: SvgIcons.Wallet,
 };
-
-const DexTabs = () => {
-  // const [modalOpen, setModalOpen] = useState(false);
+const DexStack = () => {
   return (
-    <View style={{ flex: 1 }}>
-      {/* <DexModal visible={modalOpen} onRequestClose={() => setModalOpen(false)} /> */}
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-          header: DexHeader,
-          tabBarOptions: {
-            showLable: false,
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Stack.Screen component={DexTabs} name="__Tabs" />
+      <Stack.Screen
+        component={DexModal}
+        options={{ presentation: 'transparentModal' }}
+        name="__Modal"
+      />
+    </Stack.Navigator>
+  );
+};
+const DexTabs = ({ navigation }: StackScreenProps<DexModalStackParamList>) => {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        header: DexHeader,
+        tabBarOptions: {
+          showLable: false,
+        },
+        tabBarStyle: {
+          backgroundColor: colors.BrandYellow,
+        },
+        title: route.name.replace('DEX__', ''),
+        tabBarLabel: props => {
+          return (
+            <Text style={{ color: props.color }}>
+              {route.name === 'DEX__Modal' ? null : route.name.replace('DEX__', '')}
+            </Text>
+          );
+        },
+        tabBarInactiveTintColor: '#500404',
+        tabBarActiveTintColor: '#b77409',
+        tabBarItemStyle: {
+          marginTop: route.name === 'DEX__Modal' ? 24 : undefined,
+        },
+        tabBarIcon: ({ color, size }) => {
+          const Icon = Name2Icon[route.name];
+          const newSize = route.name === 'DEX__Modal' ? size + 24 : size;
+          return <Icon width={newSize} height={newSize} fill={color} color={color} />;
+        },
+      })}
+    >
+      <Tab.Screen name="DEX__Home" component={DexHome} />
+      <Tab.Screen name="DEX__Trade" component={DexTrade} />
+      <Tab.Screen
+        name="DEX__Modal"
+        options={{
+          tabBarButton: props => {
+            return <Pressable {...props} onPress={() => navigation.navigate('__Modal')} />;
           },
-          tabBarStyle: {
-            backgroundColor: colors.BrandYellow,
-          },
-          title: route.name.replace('DEX__', ''),
-          tabBarLabel: props => {
-            return (
-              <Text style={{ color: props.color }}>
-                {route.name === 'DEX__Modal' ? null : route.name.replace('DEX__', '')}
-              </Text>
-            );
-          },
-          tabBarInactiveTintColor: '#500404',
-          tabBarActiveTintColor: '#b77409',
-          tabBarItemStyle: {
-            marginTop: route.name === 'DEX__Modal' ? 24 : undefined,
-          },
-          tabBarIcon: ({ color, size }) => {
-            const Icon = Name2Icon[route.name];
-            const newSize = route.name === 'DEX__Modal' ? size + 24 : size;
-            return <Icon width={newSize} height={newSize} fill={color} color={color} />;
-          },
-        })}
-      >
-        <Tab.Screen name="DEX__Home" component={DexHome} />
-        <Tab.Screen name="DEX__Trade" component={DexTrade} />
-        <Tab.Screen
-          name="DEX__Modal"
-          options={{
-            tabBarButton: props => {
-              return <Pressable {...props} onPress={() => {}} />;
-            },
-          }}
-          component={Black}
-        />
-        <Tab.Screen name="DEX__Orders" component={Black} />
-        <Tab.Screen name="DEX__Fund" component={Black} />
-      </Tab.Navigator>
-    </View>
+        }}
+        component={Black}
+      />
+      <Tab.Screen name="DEX__Orders" component={Black} />
+      <Tab.Screen name="DEX__Fund" component={Black} />
+    </Tab.Navigator>
   );
 };
 
-export type DexStackParamList = {
+export type DexDrawerParamList = {
   DEX_HOME: undefined;
   __Notifications: undefined;
   __Settings: undefined;
@@ -93,7 +118,7 @@ export type DexStackParamList = {
   DEX__Recive: undefined;
 };
 
-const Drawer = createDrawerNavigator<DexStackParamList>();
+const Drawer = createDrawerNavigator<DexDrawerParamList>();
 
 export const DexNav: React.FC = () => {
   return (
@@ -116,7 +141,7 @@ export const DexNav: React.FC = () => {
           options={{
             headerShown: false,
           }}
-          component={DexTabs}
+          component={DexStack}
         />
         <Drawer.Screen name="__Notifications" component={Black} />
         <Drawer.Screen name="__Settings" component={Black} />
