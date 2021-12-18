80e84b3f165f2959d75b98c7d02a9ba8c85772f6
Camisul <7963180+Camisul@users.noreply.github.com>
2021-12-19 01:28:50 +0300

    Refactor TradeScreen part2 (done)

--- app/screens/TradeScreen.tsx
+++ app/screens/TradeScreen.tsx
@@ -1,8 +1,20 @@
 import { useNavigation } from '@react-navigation/core';
 import throttle from 'lodash.throttle';
 import React, { useEffect, useMemo, useState } from 'react';
-import { Dimensions, Image, SafeAreaView, StyleSheet, Text, TextInput, View } from 'react-native';
+import {
+  Dimensions,
+  Image,
+  ImageStyle,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TextStyle,
+  View,
+  ViewStyle,
+} from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
+import { SvgIcons } from '../../assets';
 import { useAssetPicker } from '../components/AssetSelectModal';
 import { List } from '../components/List';
 import Loader from '../components/Loader';
@@ -138,17 +150,48 @@ const FloatingButton = ({ assets }: AssetsProp) => {
   );
 };
 
+const DarkFloatingButton = ({ assets }: AssetsProp) => {
+  return (
+    <View
+      style={{
+        backgroundColor: '#fff',
+        borderRadius: 4,
+        padding: 8,
+        paddingHorizontal: 12,
+      }}
+    >
+      <TouchableOpacity
+        onPress={() => {
+          const aMax = assets.A.asset.amount;
+          assets.A.setAmount(aMax.toString());
+          assets.B.formUsdt(assets.A.toUsdt(aMax));
+        }}
+      >
+        <View style={{ flexDirection: 'row' }}>
+          <SvgIcons.Wallet width={18} height={18} fill={colors.BrandYellow} />
+          <Text style={{ textAlign: 'center', fontWeight: '700', marginLeft: 8 }}>MAX</Text>
+        </View>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+type DM<T> = { darkMode?: boolean } & T;
 interface AssetProp {
   asset: theAsset;
 }
 
-const AssetDisplay = ({ asset }: AssetProp) => {
+const AssetDisplay = ({ asset, darkMode }: DM<AssetProp>) => {
+  const darkStyle = optStyleFactory(darkMode);
+
   return (
     <TouchableOpacity onPress={() => asset.open()}>
       <View style={styles.rowCenter}>
         <Image style={styles.assetIcon} source={asset.asset._asset.icon /*Bruh wtf is dis */} />
         <View>
-          <Heading style={styles.font18x500}>{asset.asset.symbol}</Heading>
+          <Heading style={darkStyle({ color: '#fff' }, styles.font18x500)}>
+            {asset.asset.symbol}
+          </Heading>
           <TextSecondary style={styles.font14}>BNB</TextSecondary>
         </View>
       </View>
@@ -156,9 +199,11 @@ const AssetDisplay = ({ asset }: AssetProp) => {
   );
 };
 
-const AmountInput = ({ asset }: AssetProp) => {
+const AmountInput = ({ asset, darkMode }: DM<AssetProp>) => {
   const [amt, setAmt] = useState(asset.amount);
   const [usd, setUsd] = useState(asset.toUsdt().toFixed(2));
+
+  const darkStyle = optStyleFactory(darkMode);
 
   useEffect(() => {
     setAmt(asset.amount);
@@ -176,7 +221,7 @@ const AmountInput = ({ asset }: AssetProp) => {
   return (
     <View>
       <TextInput
-        style={styles.amountInput}
+        style={darkStyle({ color: '#fff' }, styles.amountInput)}
         keyboardType="numeric"
         onChangeText={t => {
           setAmt(t);
@@ -187,14 +232,14 @@ const AmountInput = ({ asset }: AssetProp) => {
         value={amt}
       />
       <View style={styles.rowEnd}>
-        <TextSecondary style={styles.usdtLable}>US$</TextSecondary>
+        <TextSecondary style={darkStyle({ color: '#fff' }, styles.usdtLable)}>US$</TextSecondary>
         <TextInput
           onChangeText={t => {
             setUsd(t);
             throttled(t);
           }}
           value={usd}
-          style={styles.usdInput}
+          style={darkStyle({ color: '#fff' }, styles.usdInput)}
         />
       </View>
     </View>
@@ -256,7 +301,21 @@ const makeMessage = (assets: ScreenAssets) =>
   ' to ' +
   `${assets.B.amount} ${assets.A.asset.symbol}`;
 
-const TradeScreen: React.FC = () => {
+interface Props {
+  darkMode?: boolean;
+}
+type kindaStyle = Partial<typeof styles> | ViewStyle | TextStyle | ImageStyle;
+
+const optStyleFactory =
+  (darkMode?: boolean) =>
+  (x: kindaStyle, defaults?: kindaStyle): any => {
+    if (darkMode) {
+      return [defaults, x];
+    }
+    return [defaults];
+  };
+
+const TradeScreen: React.FC<Props> = ({ darkMode }) => {
   const allAssets = useAssets();
   const avaliableAssets = useMemo(
     () => allAssets?.assetsWithBalance.sort((a, b) => a.symbol.localeCompare(b.symbol)),
@@ -280,40 +339,70 @@ const TradeScreen: React.FC = () => {
   if (allAssets === null || !avaliableAssets) {
     return <Loader />;
   }
+  const DarkMode: React.FC = ({ children }) => <>{darkMode ? children : null}</>;
 
+  const darkStyle = optStyleFactory(darkMode);
+
+  const LightMode: React.FC = ({ children }) => <>{darkMode ? null : children}</>;
   return (
-    <SafeAreaView>
-      <Backdrop />
+    <SafeAreaView style={darkStyle(styles.darkRoot)}>
+      <LightMode>
+        <Backdrop />
+      </LightMode>
       <Modal />
       <LoaderModal />
       <SuccessModal onClose={() => nav.goBack()} />
       <View>
-        <FloatingButton assets={assets} />
-        <List style={styles.listStyle}>
-          <View style={styles.listView}>
-            <Text style={styles.listHeading}>Convert</Text>
+        <LightMode>
+          <FloatingButton assets={assets} />
+        </LightMode>
+        <DarkMode>
+          <View style={{ margin: 18 }} />
+        </DarkMode>
+        <List style={darkStyle(styles.darkList, styles.listStyle)}>
+          <View style={darkStyle(styles.darkListView, styles.listView)}>
+            <View style={[styles.rowJustifyBetween, styles.center, { paddingVertical: 8 }]}>
+              <Text style={darkStyle({ color: colors.BrandYellow }, styles.listHeading)}>
+                Convert
+              </Text>
+              <DarkMode>
+                <DarkFloatingButton assets={assets} />
+              </DarkMode>
+            </View>
             <View style={styles.rowJustifyBetween}>
-              <AssetDisplay asset={assets.A} />
-              <AmountInput asset={assets.A} />
+              <AssetDisplay darkMode={darkMode} asset={assets.A} />
+              <AmountInput darkMode={darkMode} asset={assets.A} />
             </View>
           </View>
           <View style={{ padding: 16 }}>
-            <Text style={styles.listHeading}>To</Text>
+            <Text style={darkStyle({ color: colors.BrandYellow }, styles.listHeading)}>To</Text>
             <View style={styles.rowJustifyBetween}>
-              <AssetDisplay asset={assets.B} />
-              <AmountInput asset={assets.B} />
+              <AssetDisplay darkMode={darkMode} asset={assets.B} />
+              <AmountInput darkMode={darkMode} asset={assets.B} />
             </View>
           </View>
         </List>
       </View>
-      <View style={styles.buttonContainer}>
+      <LightMode>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity onPress={fn}>
+            <View style={styles.button}>
+              <Text style={styles.buttonText}>Exchange</Text>
+            </View>
+          </TouchableOpacity>
+          <Text>The spread for this transaction is 0.423%</Text>
+        </View>
+      </LightMode>
+      <DarkMode>
+        <View style={[styles.center, styles.m12]}>
+          <Text style={{ color: '#fff' }}>The spread for this transaction is 0.423%</Text>
+        </View>
         <TouchableOpacity onPress={fn}>
-          <View style={styles.button}>
-            <Text style={styles.buttonText}>Exchange</Text>
+          <View style={styles.darkBtnView}>
+            <Text style={styles.font18x500}>Convert</Text>
           </View>
         </TouchableOpacity>
-        <Text>The spread for this transaction is 0.423%</Text>
-      </View>
+      </DarkMode>
     </SafeAreaView>
   );
 };
@@ -383,4 +472,31 @@ const styles = StyleSheet.create({
       },
     },
   ),
+  // Dark mode
+  darkRoot: { backgroundColor: '#320001', height: '100%' },
+  darkBtnView: {
+    backgroundColor: colors.BrandYellow,
+    alignItems: 'center',
+    padding: 18,
+    margin: 24,
+    borderRadius: 8,
+  },
+  darkListView: {
+    padding: 0,
+    margin: 16,
+    paddingBottom: 16,
+    marginBottom: 0,
+    borderBottomWidth: 1,
+    borderBottomColor: colors.BrandYellow,
+  },
+  darkList: {
+    backgroundColor: '#3f0000',
+    borderRadius: 18,
+  },
+  center: {
+    alignItems: 'center',
+  },
+  m12: {
+    margin: 12,
+  },
 });
