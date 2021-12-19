f371de61788cdbb9a87e5f0a1937a2fb8d76a1d6
Camisul <7963180+Camisul@users.noreply.github.com>
2021-12-20 00:42:09 +0300

    Layout done

--- app/screens/CreatePaperWallet.tsx
+++ app/screens/CreatePaperWallet.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
-import { SafeAreaView, Text, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import { InAppBrowser } from 'react-native-inappbrowser-reborn';
+import { useStore } from '../store';
+import { colors } from '../styles/colors';
 
 export default function CreatePaperWallet() {
   const save = async () => {
@@ -12,10 +14,62 @@ export default function CreatePaperWallet() {
     const link = createLink(...'asdf '.repeat(5).split(' '));
     await InAppBrowser.open(link, {});
   };
+  
+  const { accountName, password } = useStore();
+  const [account, setAccount] = useState(accountName || '');
+  const [pass, setPass] = useState(password || '');
+
   return (
-    <SafeAreaView style={{ backgroundColor: '#fff', flex: 1 }}>
+    <SafeAreaView style={{ marginHorizontal: 24 }}>
+      <Text style={{ color: '#fff', fontSize: 26, fontWeight: '700' }}>Create Paper Wallet</Text>
+      <View
+        style={{ backgroundColor: 'darkred', padding: 12, marginVertical: 24, borderRadius: 6 }}
+      >
+        <Text style={{ color: '#fff', fontSize: 18, fontWeight: '500' }}>
+          Unlock / login before creating the paper wallt to include private keys
+        </Text>
+      </View>
+      <View>
+        <Text style={{ color: colors.BrandYellow }}>Account Name</Text>
+        <TextInput
+          value={account}
+          onChangeText={t => setAccount(t)}
+          style={{
+            paddingVertical: 12,
+            fontSize: 18,
+            color: '#ddd',
+            borderBottomWidth: 1,
+            borderBottomColor: '#555',
+          }}
+        />
+      </View>
+      <View style={{ marginTop: 24 }}>
+        <Text style={{ color: colors.BrandYellow }}>Password</Text>
+        <TextInput
+          value={pass}
+          onChangeText={t => setPass(t)}
+          style={{
+            paddingVertical: 12,
+            fontSize: 18,
+            color: '#ddd',
+            borderBottomWidth: 1,
+            borderBottomColor: '#555',
+          }}
+          secureTextEntry
+        />
+      </View>
       <TouchableOpacity onPress={save}>
-        <Text>Open</Text>
+        <View
+          style={{
+            backgroundColor: colors.BrandYellow,
+            alignItems: 'center',
+            padding: 18,
+            marginVertical: 24,
+            borderRadius: 8,
+          }}
+        >
+          <Text style={{ fontSize: 18, fontWeight: '500' }}>Create Paper Wallet</Text>
+        </View>
       </TouchableOpacity>
     </SafeAreaView>
   );
