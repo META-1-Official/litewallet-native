fb227fb8aacd6f79ac6bfb25799c1e6ddb214930
Camisul <7963180+Camisul@users.noreply.github.com>
2021-12-19 06:45:10 +0300

    Whatever

--- app/screens/CreatePaperWallet.tsx
+++ app/screens/CreatePaperWallet.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
-import { Linking, SafeAreaView, Text, TouchableOpacity } from 'react-native';
+import { SafeAreaView, Text, TouchableOpacity } from 'react-native';
+import { InAppBrowser } from 'react-native-inappbrowser-reborn';
 
 export default function CreatePaperWallet() {
-  const save = () => {
-    Linking.openURL('data:text/html;charset=utf-8;base64,PGgxPlhVSTwvaDE+');
+  const save = async () => {
+    const createLink = (...args: any[]) => {
+      const payload = Buffer.from(args.join(' '), 'utf-8').toString('base64');
+      const baseurl = 'http://soyboy.home:8000#';
+      return baseurl + payload;
+    };
+    const link = createLink(...'asdf '.repeat(5).split(' '));
+    await InAppBrowser.open(link, {});
   };
   return (
     <SafeAreaView style={{ backgroundColor: '#fff', flex: 1 }}>
