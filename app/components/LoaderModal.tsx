8dde87fab3b6b0369bee62fa7f1e26fa546ac994
Camisul <7963180+Camisul@users.noreply.github.com>
2022-02-28 22:34:07 +0300

    Alternative method to show loader modal

--- app/components/LoaderModal.tsx
+++ app/components/LoaderModal.tsx
@@ -1,6 +1,8 @@
+import { useNavigation } from '@react-navigation/native';
 import React, { useState } from 'react';
 import { Dimensions, Image, Modal, SafeAreaView, View } from 'react-native';
 import { loaderGif } from '../../assets';
+import { RootStackNP } from '../WalletNav';
 
 const { width, height } = Dimensions.get('screen');
 
@@ -8,6 +10,27 @@ interface LoaderModalProps {
   visible: boolean;
   onClose: (...args: any[]) => void;
 }
+
+const LoaderModalContent = () => {
+  return (
+    <SafeAreaView
+      style={{
+        backgroundColor: '#33333333',
+        width,
+        height,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <View style={{ padding: 32, backgroundColor: '#fff', borderRadius: 8 }}>
+        <Image
+          source={loaderGif}
+          style={{ width: height / 4, height: height / 4, resizeMode: 'contain' }}
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
 
 const LoaderModal: React.FC<LoaderModalProps> = ({ visible, onClose }) => {
   return (
@@ -20,22 +43,7 @@ const LoaderModal: React.FC<LoaderModalProps> = ({ visible, onClose }) => {
         onClose(!visible);
       }}
     >
-      <SafeAreaView
-        style={{
-          backgroundColor: '#33333333',
-          width,
-          height,
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <View style={{ padding: 32, backgroundColor: '#fff', borderRadius: 8 }}>
-          <Image
-            source={loaderGif}
-            style={{ width: height / 4, height: height / 4, resizeMode: 'contain' }}
-          />
-        </View>
-      </SafeAreaView>
+      <LoaderModalContent />
     </Modal>
   );
 };
@@ -50,5 +58,17 @@ export const useLoaderModal = (openDefault?: boolean) => {
     showLoader: () => setOpen(true),
     hideLoader: () => setOpen(false),
   };
+};
+
+export const useNewLoaderModal = () => {
+  const navigation = useNavigation<RootStackNP>();
+  const open = () =>
+    navigation.navigate('modal', {
+      component: LoaderModalContent,
+      props: {},
+    });
+
+  const close = () => navigation.goBack();
+  return { open, close };
 };
 export default LoaderModal;
