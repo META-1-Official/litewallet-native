cb9d1faca8acc89f50541ef79da3c479e3ee2914
Camisul <7963180+Camisul@users.noreply.github.com>
2022-03-14 20:42:53 +0300

    Lint fix

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,12 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  "plugins": [
-    ["module:react-native-dotenv", {
-      "moduleName": "@env",
-      envName: 'NETWORK',
-    }]
-  ]
+  plugins: [
+    [
+      'module:react-native-dotenv',
+      {
+        moduleName: '@env',
+        envName: 'NETWORK',
+      },
+    ],
+  ],
 };
