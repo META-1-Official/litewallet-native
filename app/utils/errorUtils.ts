45e0a7ca9ad95ab371d73cbd0e3c7c614c1f7b70
Camisul <7963180+Camisul@users.noreply.github.com>
2022-03-16 19:54:48 +0300

    Add error utils

---
+++ app/utils/errorUtils.ts
@@ -0,0 +1,42 @@
+export function isError(e: unknown): e is Error {
+  return e instanceof Error;
+}
+
+type Eparser = (e: Error) => [Error, boolean];
+export function ErrorParser(e: Error) {
+  const parsers: Eparser[] = [consumeTxInfo, InsufficientBalance];
+
+  for (const p of parsers) {
+    try {
+      const [r, fin] = p(e);
+      e = r;
+      if (fin) {
+        break;
+      }
+    } catch (_) {}
+  }
+  return e;
+}
+
+const applyMsg = (e: Error, cb: (_: string) => string, fin = false): ReturnType<Eparser> => {
+  e.message = cb(e.message).trim();
+  return [e, fin];
+};
+
+const mkMutMsg =
+  (cb: (_: string) => string): Eparser =>
+  e =>
+    applyMsg(e, cb);
+
+const mkMutMsgFinal =
+  (cb: (_: string) => string): Eparser =>
+  e =>
+    applyMsg(e, cb, true);
+
+const consumeTxInfo = mkMutMsg(m => m.split('bitshares-crypto')[0]);
+const InsufficientBalance = mkMutMsgFinal(m => {
+  if (m.indexOf('Insufficient Balance') !== -1) {
+    return 'Insufficient Balance' + m.split('Insufficient Balance')[1];
+  }
+  throw new Error('Token not found');
+});
