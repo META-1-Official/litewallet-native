bb1d41dbb8e71e6250ce56c39a8bfffdb54b9365
Camisul <7963180+Camisul@users.noreply.github.com>
2022-04-14 01:58:42 +0300

    Create SavePdf method

---
+++ app/screens/CreatePaperWallet/SavePdf.ts
@@ -0,0 +1,21 @@
+import { Platform } from "react-native";
+import ReactNativeBlobUtil from "react-native-blob-util";
+
+const fs = ReactNativeBlobUtil.fs;
+const { CacheDir, DownloadDir } = fs.dirs;
+
+export async function savePdf(dataUrl: string) {
+  const [_header, data] = dataUrl.split(',');
+
+  const targetDir = Platform.OS === 'ios' ? CacheDir : DownloadDir;
+  const fileName = `Meta1-keys-${new Date().getTime()}.pdf`;
+  const fullName = `${targetDir}/${fileName}`;
+
+  console.log(fullName);
+
+  await fs.createFile(fullName, data, 'base64');
+
+  if (Platform.OS === 'ios') {
+    ReactNativeBlobUtil.ios.previewDocument(fullName);
+  }
+}
