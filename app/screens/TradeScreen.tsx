c2f397233533a7d3fb66f5f642982931f68c9341
Camisul <7963180+Camisul@users.noreply.github.com>
2022-05-23 01:32:14 +0300

    Don't crossCheckPrice

--- app/screens/TradeScreen.tsx
+++ app/screens/TradeScreen.tsx
@@ -354,7 +354,7 @@ const crossCheckPrice = async ({ A, B }: ScreenAssets) => {
     const price = Number(A.amount) / Number(B.amount);
     const tickerPrice = Number(ticker.lowest_ask);
     const mesq = (1 / 2) * (price - tickerPrice) ** 2;
-    console.log(mesq);
+    console.log('mesq', mesq);
     return mesq < 1e-10;
   } catch (e: any) {
     console.warn(e);
@@ -385,11 +385,7 @@ const mkPerformSwap = (
       throw new Error("Can't swap the same assets");
     }
 
-    if (!(await crossCheckPrice(assets))) {
-      throw new Error(
-        'Price deviation too high, enter the exact amount to swap or try again later',
-      );
-    }
+    await crossCheckPrice(assets);
 
     await swapWithPassword(
       accountInfo,
