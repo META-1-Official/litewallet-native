009debf9094a3c7715e50274a40dd844ee765795
Camisul <7963180+Camisul@users.noreply.github.com>
2022-05-27 15:15:05 +0300

    Update build scripts

--- buildIOSRelease.sh
+++ buildIOSRelease.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
-cd ios 
-if ! which xcbeautify > /dev/null; then
-  xcodebuild -workspace nativeapp.xcworkspace -configuration Release -scheme nativeapp archive
-else
-  xcodebuild -workspace nativeapp.xcworkspace -configuration Release -scheme nativeapp archive | xcbeautify
+
+
+if [ -z ${NETWORK+x} ]; then
+  source .env;
 fi
+
+echo "Network $NETWORK"
+
+cd ios 
+
+NOW=$NETWORK_$(date +"%m_%d_%YT%H-%M")
+if ! which xcbeautify > /dev/null; then
+  PIPE=tee
+else
+  PIPE=xcbeautify
+fi
+
+xcodebuild -workspace nativeapp.xcworkspace -configuration Release -scheme nativeapp -archivePath ./dist/ archive | $PIPE && \
+mkdir -p ../dist/ios-$NOW && \
+xcodebuild -exportArchive -archivePath dist.xcarchive -exportOptionsPlist exportOptions.plist -exportPath ../dist/ios-$NOW | $PIPE
