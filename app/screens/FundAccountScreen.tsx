a2f3ace8c69c1c4cd44873572e85918ca5c73792
Camisul <7963180+Camisul@users.noreply.github.com>
2022-06-01 17:15:13 +0300

    Block onramper trackers

--- app/screens/FundAccountScreen.tsx
+++ app/screens/FundAccountScreen.tsx
@@ -9,6 +9,12 @@ const FundAccount: React.FC<{}> = () => {
         source={{
           uri: 'https://widget.onramper.com/?color=ffc000&defaultAmount=1000&defaultFiat=USD&defaultCrypto=BTC&apiKey=pk_prod_k6LKERIMdGDE8geCxOApKSCy6mnfF5CuhI4TLZj55Wc0',
         }}
+        onShouldStartLoadWithRequest={req => {
+          //prettier-ignore
+          return !req.url.includes('google-analytics.com')
+              && !req.url.includes('googletagmanager')
+              && !req.url.includes('sentry.io');
+        }}
       />
     </SafeAreaView>
   );
