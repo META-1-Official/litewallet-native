77f9b9895a7fe36f32f2d6d0533893aa828d8622
Camisul <7963180+Camisul@users.noreply.github.com>
2022-06-17 03:54:32 +0300

    Actually block the outgoing requests to google analytics

--- app/screens/FundAccountScreen.tsx
+++ app/screens/FundAccountScreen.tsx
@@ -10,6 +10,9 @@ const FundAccount: React.FC<{}> = () => {
           uri: 'https://widget.onramper.com/?color=ffc000&defaultAmount=1000&defaultFiat=USD&defaultCrypto=BTC&apiKey=pk_prod_k6LKERIMdGDE8geCxOApKSCy6mnfF5CuhI4TLZj55Wc0',
         }}
         onShouldStartLoadWithRequest={req => {
+          /** NOT WORKING; Actual implementations is in the patch
+           * @see {@link this patches/react-native-webview+11.17.2.patch}
+           */
           //prettier-ignore
           return !req.url.includes('google-analytics.com')
               && !req.url.includes('googletagmanager')
