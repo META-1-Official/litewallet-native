ac858a54daacde330a330aec1eb3f3ae05c21a13
Royalone94 <stuntblitz@gmail.com>
2023-01-17 23:36:19 -0800

    implement password validation when upload or remove avatar

--- app/screens/SettingsScreen/AvatarGroup.tsx
+++ app/screens/SettingsScreen/AvatarGroup.tsx
@@ -4,29 +4,36 @@ import { launchImageLibrary } from 'react-native-image-picker';
 import useAppDispatch from '../../hooks/useAppDispatch';
 import { deleteAvatar, uploadAvatar } from '../../store/wallet/wallet.actions';
 import { isError } from '../../utils/errorUtils';
-import { getPassword } from '../../utils';
+import { getPassword, catchError } from '../../utils';
 import ListItem from './ListItem';
+import useAppSelector from '../../hooks/useAppSelector';
+import {
+  _login
+} from '../../services/meta1Api';
 
-const upload = async (dispatch: any) => {
+const upload = async (dispatch: any, accountName: string) => {
   try {
     const passwd = await getPassword();
     if (!passwd) {
       return;
     }
-    const result = await launchImageLibrary({
-      mediaType: 'photo',
-      maxHeight: 500,
-      maxWidth: 500,
-      quality: 0.2,
+    catchError(async () => {
+      await _login(accountName, passwd);
+      const result = await launchImageLibrary({
+        mediaType: 'photo',
+        maxHeight: 500,
+        maxWidth: 500,
+        quality: 0.2,
+      });
+
+      const image = result.assets?.[0]!;
+      console.log(result);
+      if (result.didCancel) {
+        return;
+      }
+
+      dispatch(uploadAvatar({ image }));
     });
-
-    const image = result.assets?.[0]!;
-    console.log(result);
-    if (result.didCancel) {
-      return;
-    }
-
-    dispatch(uploadAvatar({ image }));
   } catch (e) {
     if (isError(e)) {
       console.warn(e.message);
@@ -38,23 +45,28 @@ const upload = async (dispatch: any) => {
 };
 
 const AvatarGroup = () => {
+  const { accountName } = useAppSelector(state => state.wallet);
   const dispatch = useAppDispatch();
   const handleDeleteAvatar = async () => {
     const passwd = await getPassword();
     if (!passwd) {
       return;
     }
-    Alert.alert('Remove avatar', 'Are you sure you want to continue', [
-      {
-        text: 'Cancel',
-        onPress: () => false,
-        style: 'cancel',
-      },
-      {
-        text: 'OK',
-        onPress: () => dispatch(deleteAvatar()),
-      },
-    ]);
+
+    catchError(async () => {
+      await _login(accountName, passwd);
+      Alert.alert('Remove avatar', 'Are you sure you want to continue', [
+        {
+          text: 'Cancel',
+          onPress: () => false,
+          style: 'cancel',
+        },
+        {
+          text: 'OK',
+          onPress: () => dispatch(deleteAvatar()),
+        },
+      ]);
+    });
   };
 
   return (
@@ -62,7 +74,7 @@ const AvatarGroup = () => {
       <Text style={{ color: '#fff', fontSize: 26, fontWeight: '600', marginBottom: 8 }}>
         Avatar
       </Text>
-      <ListItem text="Upload" onPress={() => upload(dispatch)} />
+      <ListItem text="Upload" onPress={() => upload(dispatch, accountName)} />
       <ListItem text="Remove" color="#E03616" onPress={handleDeleteAvatar} />
     </View>
   );
