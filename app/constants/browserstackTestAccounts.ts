aae2ca5ebb9c72a743d1caa3a50d10f7fc7a7454
Aleksandr Ufimtsev <fimak@bk.ru>
2023-03-29 13:41:45 +0700

    feature: disabling faceki for list of qa wallets (#88)

---
+++ app/constants/browserstackTestAccounts.ts
@@ -0,0 +1,10 @@
+export const browserstackTestAccounts = [
+  'gem-1',
+  'test-automation',
+  'john-doe',
+  'olive-5',
+  'marry-14',
+  'mary-14',
+  'bond-03',
+  'rock-64',
+];
--- app/screens/CustomProviders/CustomProvidersScreen.tsx
+++ app/screens/CustomProviders/CustomProvidersScreen.tsx
@@ -4,9 +4,14 @@ import { LOGIN_PROVIDER } from '@web3auth/react-native-sdk';
 // import { SdkLoginParams } from '@web3auth/react-native-sdk/src/types/sdk';
 import React, { useState } from 'react';
 import { Animated, Text, View, SafeAreaView, ScrollView } from 'react-native';
+import Toast from 'react-native-toast-message';
 import { RootStackParamList } from '../../AuthNav';
-import { useAppDispatch } from '../../hooks';
+import { browserstackTestAccounts } from '../../constants/browserstackTestAccounts';
+import { useAppDispatch, useAppSelector } from '../../hooks';
 import useAnimatedKeyboard from '../../hooks/useAnimatedKeyboard';
+import { useStore } from '../../store';
+import { login } from '../../store/signIn/signIn.actions';
+import { authorize } from '../../store/wallet/wallet.reducers';
 import { getWeb3User } from '../../store/web3/web3.actions';
 // import CountryNumber from './CountryNumber';
 import styles from './CustomProvidersScreen.styles';
@@ -20,12 +25,14 @@ type Props = NativeStackScreenProps<RootStackParamList, 'CustomProviders'>;
 
 const CustomProvidersScreen: React.FC<Props> = ({ navigation }) => {
   const dispatch = useAppDispatch();
+  const auth = useStore(state => state.authorize);
   // const { mobile } = useAppSelector(state => state.signUp);
   // const rawMobile = mobile?.split(/\s+/).join('').replace('+', '');
   const [providers, setProviders] = useState(providersListSmall);
   const isSmall = providers.length === providersListSmall.length;
   const [email, setEmail] = useState('');
   const [emailError, setEmailError] = useState(false);
+  const { accountName } = useAppSelector(state => state.signIn);
   // const [phone, setPhone] = useState(rawMobile?.slice(-10) || '');
   // const [countryCode, setCountryCode] = useState(rawMobile?.slice(0, -10) || '1');
   // const [phone, setPhone] = useState('');
@@ -45,6 +52,24 @@ const CustomProvidersScreen: React.FC<Props> = ({ navigation }) => {
   //   setPhone(String(parseInt(text, 10) || ''));
   // };
 
+  const loginToAppHandler = () => {
+    dispatch(login({ accountName, email }))
+      .unwrap()
+      .then(loginDetails => {
+        console.log('Logged in successfully! loginDetails: ', loginDetails);
+        dispatch(authorize({ accountName, email, token: loginDetails.token }));
+        auth();
+      })
+      .catch(error => {
+        Toast.show({
+          type: 'error',
+          text1: 'Something went wrong!',
+          text2: 'Try to login again.',
+        });
+        console.error(error);
+      });
+  };
+
   const handleLogin = (loginProvider: string, login = '') => {
     dispatch(
       getWeb3User({
@@ -59,7 +84,11 @@ const CustomProvidersScreen: React.FC<Props> = ({ navigation }) => {
       .unwrap()
       .then(web3AuthData => {
         if (web3AuthData.privateKey) {
-          navigation.navigate('FaceKI');
+          if (browserstackTestAccounts.includes(accountName)) {
+            loginToAppHandler();
+          } else {
+            navigation.navigate('FaceKI');
+          }
         }
       })
       .catch(error => console.error(error));
