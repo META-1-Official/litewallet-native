faebf7e257bcb89b3632b62486636e002ca65410
Aleksandr Ufimtsev <fimak@bk.ru>
2023-10-19 17:01:27 +0600

    Revert "mfa diabling"
    This reverts commit f225767d87ea3bf7ee558e872c67a4a51b8eaea0.

--- app/services/web3.services.ts
+++ app/services/web3.services.ts
@@ -1,4 +1,3 @@
-import { MFA_LEVELS } from '@toruslabs/openlogin/src/constants';
 import * as WebBrowser from '@toruslabs/react-native-web-browser';
 import Web3Auth, { OPENLOGIN_NETWORK_TYPE } from '@web3auth/react-native-sdk';
 import EncryptedStorage from 'react-native-encrypted-storage';
@@ -10,11 +9,8 @@ const resolvedRedirectUrl = 'io.meta1.appbeta://auth';
 const web3auth: Web3Auth = new Web3Auth(WebBrowser, EncryptedStorage, {
   clientId: config.WEB3_CLIENT_ID,
   network: config.WEB3_NETWORK as OPENLOGIN_NETWORK_TYPE,
-  redirectUrl: resolvedRedirectUrl,
   whiteLabel: {
-    appName: 'META1',
-    logoLight: 'https://pbs.twimg.com/profile_images/980143928769839105/hK3RnAff_400x400.jpg',
-    defaultLanguage: 'en',
+    name: 'META1',
   },
 });
 
@@ -22,12 +18,7 @@ export const web3Login = async (options: SdkLoginParams): Promise<Web3Auth> => {
   await web3auth.init();
   await web3auth.login({
     ...options,
-    mfaLevel: MFA_LEVELS.NONE,
     redirectUrl: resolvedRedirectUrl,
-    extraLoginOptions: {
-      ...options.extraLoginOptions,
-      redirect_uri: resolvedRedirectUrl,
-    },
   });
   return web3auth;
 };
